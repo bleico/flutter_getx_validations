8f53e63adf539d2fa770aab13bc961ea9c10f9f5
bleico <bleiderc@gmail.com>
2021-08-27 13:59:26 -0400

    usando el worker ever

--- lib/controllers/socket_client_controller.dart
+++ lib/controllers/socket_client_controller.dart
@@ -5,9 +5,10 @@ import 'package:get/state_manager.dart';
 
 class SocketClientController extends GetxController {
   RxString _message = "".obs;
+  RxInt _counter = 0.obs;
   RxString get message => _message;
 
-  late Timer _timer;
+  late Timer _timer, _timerCounter;
 
   final _faker = Faker();
 
@@ -18,8 +19,20 @@ class SocketClientController extends GetxController {
   }
 
   _init() {
+    // Usando el worker ever
+    ever(
+      _counter,
+      (_) {
+        print("_counter ha sido cambiado ${_counter.value}");
+      },
+    );
+
     _timer = Timer.periodic(Duration(seconds: 5), (timer) {
       _message.value = _faker.lorem.sentence();
+    });
+
+    _timerCounter = Timer.periodic(Duration(seconds: 1), (timer) {
+      _counter.value++;
     });
   }
 
@@ -27,6 +40,9 @@ class SocketClientController extends GetxController {
   void onClose() {
     if (_timer != null) {
       _timer.cancel();
+    }
+    if (_timerCounter != null) {
+      _timerCounter.cancel();
     }
     super.onClose();
   }
